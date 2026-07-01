ec3a92605f53680d1cf054d34c3f4dfcfcf608eb
AmitojSingh78 <amitojsingh6666@gmail.com>
2026-07-01 20:44:23 +0530

    Installed dependencies and created first Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Customer Behavior Analytics",
+    layout="wide"
+)
+st.title("Customer Behavior Analytics Dashboard")
+st.write("Welcome to the Customer Behavior Analytics Project!")
+st.success("Project setup is successful.")
